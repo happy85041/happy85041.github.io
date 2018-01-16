a5f4fc17ab81e08abf3e77637f9a7628430ccbb8
viral prajapati <happy85041@gmail.com>
2018-01-16 17:23:33 +0530

    chnage theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,135 @@
-(function ($) {
+/*
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-	$(".hero-area-slider").owlCarousel({
-		items: 1,
-		loop: true,
-		dots: true,
-		animateIn: "fadeIn",
-		animateOut: "fadeOut",
-		mouseDrag: false,
+(function($) {
+
+	var settings = {
+
+		// Parallax background effect?
+			parallax: true,
+
+		// Parallax factor (lower = more intense, higher = less intense).
+			parallaxFactor: 20
+
+	};
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1800px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
 	});
 
-	$(".testimonial-slider").owlCarousel({
-		items: 1,
-		loop: true,
-		dots: true,
+	$(function() {
+
+		var $window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer'),
+			$main = $('#main');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Touch?
+			if (skel.vars.mobile) {
+
+				// Turn on touch mode.
+					$body.addClass('is-touch');
+
+				// Height fix (mostly for iOS).
+					window.setTimeout(function() {
+						$window.scrollTop($window.scrollTop() + 1);
+					}, 0);
+
+			}
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Footer.
+			skel.on('+medium', function() {
+				$footer.insertAfter($main);
+			});
+
+			skel.on('-medium !medium', function() {
+				$footer.appendTo($header);
+			});
+
+		// Header.
+
+			// Parallax background.
+
+				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+					if (skel.vars.browser == 'ie'
+					||	skel.vars.mobile)
+						settings.parallax = false;
+
+				if (settings.parallax) {
+
+					skel.on('change', function() {
+
+						if (skel.breakpoint('medium').active) {
+
+							$window.off('scroll.strata_parallax');
+							$header.css('background-position', 'top left, center center');
+
+						}
+						else {
+
+							$header.css('background-position', 'left 0px');
+
+							$window.on('scroll.strata_parallax', function() {
+								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+							});
+
+						}
+
+					});
+
+					$window.on('load', function() {
+						$window.triggerHandler('scroll');
+					});
+
+				}
+
+		// Main Sections: Two.
+
+			// Lightbox gallery.
+				$window.on('load', function() {
+
+					$('#two').poptrox({
+						caption: function($a) { return $a.next('h3').text(); },
+						overlayColor: '#2c2c2c',
+						overlayOpacity: 0.85,
+						popupCloserText: '',
+						popupLoaderText: '',
+						selector: '.work-item a.image',
+						usePopupCaption: true,
+						usePopupDefaultStyling: false,
+						usePopupEasyClose: false,
+						usePopupNav: true,
+						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+					});
+
+				});
+
 	});
 
-	// init Isotope
-	var $grid = $('.portfolio-container').isotope({
-		masonry: {
-			gutter: 10,
-		}
-	});
-	// filter items on button click
-	$('.portfolio-filter').on('click', 'li', function () {
-		var filterValue = $(this).attr('data-filter');
-		$grid.isotope({
-			filter: filterValue
-		});
-	});
-
-	//On click change menu color
-	$(".navbar-nav").on("click", "li", function () {
-		$(".navbar-nav li").removeClass("active");
-		$(this).addClass("active");
-	});
-
-	$('.scrl-down').on('click', function (e) {
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top
-		}, 1000, 'linear');
-	});
-
-
-	//On scroll menu fixed to top
-	var menu = $("nav");
-	$(window).scroll(function () {
-		//more then or equals to
-		if ($(window).scrollTop() < 800) {
-			menu.removeClass('fixed-top animated slideInDown');
-
-			//less then 100px from top
-		} else {
-			menu.addClass('fixed-top animated slideInDown');
-
-		}
-	});
-
-//	$('.scrollbar-body').mCustomScrollbar({
-//		theme: "dark",
-//		mouseWheel: {
-//			scrollAmount: 150
-//		},
-//		setHeight: '100%',
-//	});
-
-}(jQuery));
+})(jQuery);
